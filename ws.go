2782d47af3ab21c94547a5b32a6036a0c9a16518
Alvee <m.alvee8141@gmail.com>
2024-06-17 01:16:33 +0600

    remove duplicate hanlder

--- ws.go
+++ ws.go
@@ -40,23 +40,3 @@ const (
 	PingInterval = 5 * time.Second
 	PingWait     = 10 * time.Second
 )
-
-type Handler struct{}
-
-func (c *Handler) OnOpen(socket *gws.Conn) {
-	_ = socket.SetDeadline(time.Now().Add(PingInterval + PingWait))
-}
-
-func (c *Handler) OnClose(socket *gws.Conn, err error) {}
-
-func (c *Handler) OnPing(socket *gws.Conn, payload []byte) {
-	_ = socket.SetDeadline(time.Now().Add(PingInterval + PingWait))
-	_ = socket.WritePong(nil)
-}
-
-func (c *Handler) OnPong(socket *gws.Conn, payload []byte) {}
-
-func (c *Handler) OnMessage(socket *gws.Conn, message *gws.Message) {
-	defer message.Close()
-	socket.WriteMessage(message.Opcode, message.Bytes())
-}
