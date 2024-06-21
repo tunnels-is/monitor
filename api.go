344ee19225263aa4e64c573b9793ff421b11bde7
Alvee <m.alvee8141@gmail.com>
2024-06-21 23:12:43 +0600

    Update api.go

--- api.go
+++ api.go
@@ -63,58 +63,6 @@ func FindDatacenter(c echo.Context) error {
 
 
 
-func ListDatacenters(c echo.Context) error {
-	dataCenters := make([]*Datacenter, 0)
-
-	var errs []error
-	action := func(path string) {
-		if !strings.Contains(path, "datacenter_") {
-			return
-		}
-		data, err := GetObjectFullPath(path)
-		dc := new(Datacenter)
-		err = json.Unmarshal(data, dc)
-		if err != nil {
-			errs = append(errs, err)
-			return
-		}
-		dataCenters = append(dataCenters, dc)
-	}
-
-	err := GetObjects(basePath, action)
-	if err != nil {
-		return c.JSON(500, err)
-	}
-
-	if len(errs) != 0 {
-		return c.JSON(500, errs)
-	}
-
-	return c.JSON(200, dataCenters)
-}
-
-func FindDatacenter(c echo.Context) error {
-	id := c.Param("id")
-	idInt, err := strconv.Atoi(id)
-	if err != nil {
-		return c.JSON(400, err)
-	}
-
-	data, err := GetObject(DataCenterPath, DataCenterFile, idInt)
-	if err != nil {
-		return c.JSON(500, err)
-	}
-
-	DC := new(Datacenter)
-	err = json.Unmarshal(data, DC)
-	if err != nil {
-		return c.JSON(500, err)
-	}
-
-	return c.JSON(200, DC)
-}
-
-
 func UserConfigRecieveHandler(c echo.Context) error {
 	var config SocketConfig
 	if err := c.Bind(&config); err != nil {
